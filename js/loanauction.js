c45c07cf04d7dba29083cc8ecab8d3b5c777f198
LoneRifle <LoneRifle@users.noreply.github.com>
2015-01-18 17:43:11 +0800

    Refactor, activate only if bid_summary active
    Refactor to use jquery where suitable
    
    Trigger the chart render only if bid summary is the active tab when the
    page loads
    
    Discard all table entries pertaining to bid groups. A future commit will
    attempt to track those groups (usually holding information about where
    the user's bid is relative to everyone for the rate that was bid) on the
    chart.
    
    TODO: hook into logic that populates the Bid Summary tab, flesh out
    functionality to toggle between bar, pie and no chart.

--- js/loanauction.js
+++ js/loanauction.js
@@ -2,55 +2,63 @@
  * FCViz 
  * Seed script for loan auction pages
  */
- 
-var tableDiv = document.getElementById("bids_summary");
-var tableOrig = tableDiv.firstElementChild;
+var bidSummaryTab = $("#bids-summary")[0];
 
-//Clone the table and hide it so that we can make it useful to createChart
-var table = tableOrig.cloneNode(true);
-table.id = "bids_summary_data";
-var header = table.getElementsByTagName("thead")[0].children[0];
-//Remove the status header
-header.removeChild(header.lastElementChild);
-//Trim the header text to ensure that createChart can find them
-header.firstElementChild.textContent = header.firstElementChild.textContent.trim();
-header.lastElementChild.textContent = header.lastElementChild.textContent.trim();
-
-var rows = table.getElementsByTagName("tbody")[0];
-
-//Trim all bid-groups from the table
-var bidGroups = rows.getElementsByClassName("sub-accepted sub-group");
-for (var i = 0; i < bidGroups.length; ++i) {
-  rows.removeChild(bidGroups[i]);
+if (bidSummaryTab.attributes["class"].value.indexOf("active") != -1) {
+  renderBidSummaryCharts();
 }
+  
+function renderBidSummaryCharts() {
+  var tableOrig = $("#bids_summary").find("table");
 
-var data = rows.children;
-//Remove the third column of the table since it only contains the liveness of the bids at that rate
-for (var i = 0; i < data.length; ++i) {
-  data[i].removeChild(data[i].getElementsByClassName("status")[0]);
-  data[i].firstElementChild.textContent = data[i].firstElementChild.textContent.trim();
-  data[i].lastElementChild.textContent = data[i].lastElementChild.textContent.trim();
+  //Clone the table and hide it so that we can make it useful to createChart
+  var table = tableOrig.clone()[0];
+  table.id = "bids_summary_data";
+  table.style.visibility = "hidden";  
+
+  var header = jQuery(table).find("th");
+  //Trim the header text to ensure that createChart can find them
+  header.each(function(){ $(this).text( $(this).text().trim()); });
+  //Remove the status header
+  header.last().remove();
+    
+  var rows = jQuery(table).find("tbody");
+
+  //Trim all bid-groups from the table
+  //TODO: iterate through the bid-groups and note information about the user's bid.
+  //Interpolate that on the chart.
+  var bidGroups = rows.find(".sub-accepted, .sub-group").remove();
+
+  //Remove the column of the table since it containing the status of the bids at that rate
+  //Capture the rate found in the first element (because FC may mangle the rate 
+  //when including user bids), then trim the last element
+  var rateRegexp = /\d+\.\d%/;
+  rows.children().each(function(){
+    $(this).find(".status").remove();
+    var f = $(this).find("td").first();
+    f.text( rateRegexp.exec(f.text())[0] );
+    var l = $(this).find("td").last();
+    l.text( l.text().trim() );
+  });
+
+  //Create a div to hold the chart and related controls
+  var chartDiv = document.createElement("div");
+  chartDiv.id = "bids_summary_chart";
+
+  var chartControlDiv = document.createElement("div");
+  chartControlDiv.id = "bids_summary_chart_control";
+  chartControlDiv.appendChild(document.createTextNode("Bar | Pie | None"));
+  chartControlDiv.appendChild(chartDiv);
+
+  tableOrig.before(chartControlDiv);
+  $("#bids_summary").append(table);
+
+  //Generate the chart based on the data found in table
+  createChart('bar', '#'+table.id, 'Amount', {
+    "targetDiv": '#'+chartDiv.id,
+    "showTable": true,
+    "labels": "Rate",
+    "addComma": true,
+    "prefix": "£"
+  });
 }
-
-table.style.visibility = "hidden";
-
-//Create a div to hold the chart and related controls
-var chartDiv = document.createElement("div");
-chartDiv.id = "bids_summary_chart";
-
-var chartControlDiv = document.createElement("div");
-chartControlDiv.id = "bids_summary_chart_control";
-chartControlDiv.appendChild(document.createTextNode("Bar | Pie | None"));
-chartControlDiv.appendChild(chartDiv);
-
-tableDiv.insertBefore(chartControlDiv,tableOrig);
-tableDiv.appendChild(table);
-
-//Generate the chart based on the data found in table
-createChart('bar', '#'+table.id, 'Amount', {
-  "targetDiv": '#'+chartDiv.id,
-  "showTable": true,
-  "labels": "Rate",
-  "addComma": true,
-  "prefix": "£"
-});
