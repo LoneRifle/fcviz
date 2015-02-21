92d6aed1ea277071e2cd96a80bb8a92139e958a5
LoneRifle <LoneRifle@users.noreply.github.com>
2015-02-21 21:22:41 +0800

    Embed the bid summary chart in requests page

--- js/requests.js
+++ js/requests.js
@@ -30,15 +30,47 @@ function createPreviewUnder(row) {
     .attr("id", "filler-"+row.attr("id"));
   filler.attr("style", "display: none;");
   
+  var id = "preview-"+row.attr("id");
+  
   var td = $(document.createElement("td"))
     .attr("colspan", 9)
+    .attr("id", id)
     .html("Loading...");
   
+  var href = row.find("a.mediumText").attr("href");
+  
+  var total = +row.find("td").eq(3).html().replace("£","").replace(",","");
+  
+  $.get(href, window.populatePreview.bind(window, id, total)).fail(function(jqXHR, textStatus, errorThrown) {
+    $(td).html("Failed to retrieve "+href+", chart render aborted: "+errorThrown);
+  });
+  
   var preview = $(document.createElement("tr"))
-    .attr("class", "preview")
-    .attr("id", "preview-"+row.attr("id"));
+    .attr("class", "preview");
 
   preview.append(td);
   preview.attr("style", "display: none;");
   row.after(filler,preview);
+}
+
+window.summaryVizDimensions = {
+  height: 65,
+  width: 300,
+  margin: {top: 2, right: 10, bottom: 10, left: 10}
+};
+
+window.populatePreview = function (id, total, data){
+  var dataTableStartIndex = data.indexOf("<table class='brand'>");
+  
+  var previewBidsId = "preview_bids_"+id;
+  var previewBids = $(document.createElement("span"))
+    .attr("id", previewBidsId)
+    .attr("style", "width: 300px")
+    .html(data.substring(dataTableStartIndex, data.indexOf("</table>", dataTableStartIndex)) + "</table>" );
+  previewBids.find("table").attr("style", "display: none");
+  $("#"+id).html("").append(previewBids);
+
+  window.renderBidSummaryCharts(total, "#"+id, previewBidsId);
+  $("#"+id).find("svg").find("g.tick").attr("style", "display: none;");
+  $("#"+id).find("svg").find("g").find("text").attr("style", "display: none;");
 }
