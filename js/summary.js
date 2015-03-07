8f21efdc1e4dccf86927e28508109dd1f73dc2f7
LoneRifle <LoneRifle@users.noreply.github.com>
2015-03-07 17:51:55 +0800

    Flesh out hide repaid checkbox
    On change of input, find all the loan part rows which have been repaid
    and show/hide them.
    
    TODO: Reapply table formatting.

--- js/summary.js
+++ js/summary.js
@@ -3,10 +3,10 @@
  * Payload script for summary page
  */
  
-var hideRepaid = $(document.createElement("input"))
-  .attr("id", "hide_repaid")
-  .attr("type", "checkbox");
-
+//Get rid of the pesky popover divs that get injected into place,
+//but are not needed since one is generated already.
+ $("div.popover").detach();
+ 
 //Observe mutations made to table.zebra-striped, so that we can reapply window.fcViz
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 window.fcVizObserver = new MutationObserver(function(mutations) {
@@ -14,9 +14,21 @@ window.fcVizObserver = new MutationObserver(function(mutations) {
     switch(mutation.target.id) {
       case "all_lends":
         if ($("#hide_repaid").length == 0) {
+          var hideRepaid = $(document.createElement("input"))
+            .attr("id", "hide_repaid")
+            .attr("type", "checkbox");
+          hideRepaid[0].checked = window.repaidHidden;
           var partsControls = $("div#all_lends table.zebra-striped tbody td").last();
           partsControls.children().before(hideRepaid, "Hide Repaid ");
         }
+        $("#hide_repaid").on("change", function(){
+          window.repaidHidden = this.checked;
+          $("#all_lends table.brand tbody tr:has(td:contains(Repaid))")
+            .attr("style", window.repaidHidden? "display: none" : null);
+        });
+        //After installing the checkbox, apply its change method on the table rows.
+        $("#all_lends table.brand tbody tr:has(td:contains(Repaid))")
+          .attr("style", window.repaidHidden? "display: none" : null);
         break;
       default:
         break;
