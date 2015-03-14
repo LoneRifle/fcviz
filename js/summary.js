ba25091d2b5a8c6eaab3bcf3b088007586cea920
LoneRifle <LoneRifle@users.noreply.github.com>
2015-03-14 23:09:02 +0800

    Refactor + repay graph link text
    Move the popover removal code under mutation observer so that stray
    popovers get removed with each mutation. Ensure also that the we apply
    the hide_repaid callback only with each hide_repaid installation, not
    every mutation.
    
    Add a link that users can click on to show a graph constructed from the
    repayment schedule. TODO: actually implementing the graph.

--- js/summary.js
+++ js/summary.js
@@ -3,9 +3,6 @@
  * Payload script for summary page
  */
  
-//Get rid of the pesky popover divs that get injected into place,
-//but are not needed since one is generated already.
- $("div.popover").detach();
  
 //Observe mutations made to table.zebra-striped, so that we can reapply window.fcViz
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
@@ -13,20 +10,29 @@ window.fcVizObserver = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     switch(mutation.target.id) {
       case "all_lends":
+        //Get rid of the pesky popover divs that get injected into place,
+        //but are not needed since one is generated already.
+        $("div.popover").detach();
         if ($("#hide_repaid").length == 0) {
           var hideRepaid = $(document.createElement("input"))
             .attr("id", "hide_repaid")
             .attr("type", "checkbox");
           hideRepaid[0].checked = window.repaidHidden;
           var partsControls = $("div#all_lends table.zebra-striped tbody td").last();
-          partsControls.children().before(hideRepaid, "Hide Repaid ");
+          partsControls.children().before(hideRepaid, "Hide Repaid ");          
+          $("#hide_repaid").on("change", function(){
+            window.repaidHidden = this.checked;
+            changeRepaidRowsAndReformat();
+          });
+          //After installing the checkbox, apply its change method on the table rows.
+          changeRepaidRowsAndReformat();        
         }
-        $("#hide_repaid").on("change", function(){
-          window.repaidHidden = this.checked;
-          changeRepaidRowsAndReformat();
-        });
-        //After installing the checkbox, apply its change method on the table rows.
-        changeRepaidRowsAndReformat();
+        if ($("#repay_graph_link").length == 0) {
+          var repayGraphLink = $(document.createElement("span")).html("(graph)")
+            .attr("id", "repay_graph_link")
+            .attr("class", "leftblue");
+          $("div#all_lends table.zebra-striped tbody td:first-child a:first-child").after(" ",repayGraphLink);
+        }
         break;
       default:
         break;
