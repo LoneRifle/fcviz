e8026bdb59771bb19256b58f4ec77a301ab1c29d
LoneRifle <LoneRifle@users.noreply.github.com>
2015-07-04 18:41:26 +0800

    Allow manual reload of page to enable shortcuts

--- js/sell.js
+++ js/sell.js
@@ -21,6 +21,14 @@ if (angular.resumeBootstrap) {
   angular.resumeBootstrap();
 }
 
+//Add a link that reloads the page, so that we can pick up the buttons
+var reload = angular.element(document.createElement("span"));
+reload.attr("class","repay_change_data")
+  .html("Enable Shortcuts")
+  .on("click", function(){location.reload();});
+angular.element(document.querySelector("p"))
+  .append("&nbsp;").append(reload);
+
 var app = angular.element(document.querySelector("div"));
 var $scope = app.scope();
 var SellableLoanParts = app.injector().get("SellableLoanParts");
