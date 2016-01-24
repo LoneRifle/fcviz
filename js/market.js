3e611d02200e41676778e7b628e59bb2e9dbc90a
LoneRifle <LoneRifle@users.noreply.github.com>
2016-01-24 21:56:02 +0800

    Move premium control to near price
    Rework the layout of the price and keyword controls to make room for
    premium. These two controls, along with premium, have been moved under a
    containing <li> tag, which spans m1-m3.

--- js/market.js
+++ js/market.js
@@ -1,6 +1,40 @@
+
+
+//Tag all advanced controls
+$(".advanced-filters li").attr("advanced", true);
+
+//Hide risk band checkboxes and use labels for selection
 $(".risk-bands li").each(function() { 
   $(this).attr("active", $(this).find("input[type=checkbox]").is(":checked"));
 });
 $(".risk-bands li label").click(function() {
   $(this).parent().attr("active", $(this).parent().attr("active") === "false")
 });
+
+//Rework the layout of the first form
+rearrangeKeywordPriceControls();
+
+
+function rearrangeKeywordPriceControls() {
+  var keywordPriceContainer = $(document.createElement("li"));
+  keywordPriceContainer.addClass("m1-m3");
+  $(".loan-part-filters .m1-m2").before(keywordPriceContainer);
+  
+  var targets = [
+    ["keywords", "loan-part", "m1-m2", "m1"], 
+    ["price", "loan-part", "m3", "m2"], 
+    ["premium", "advanced", "m6-m8", "m3"]
+  ];
+  
+  $(targets).each(function(index,args){
+    var target = $("." + args[1] + "-filters ." + args[2]).detach();
+    target.removeClass(args[2]).addClass(args[0]);
+    keywordPriceContainer.append(target);
+  });  
+}
+
+function convertToSpan(e) {
+  var advanced = e.attr("advanced");
+  var span = e.replaceWith("<span>"+e.html()+"</span>").attr("advanced", advanced);
+  return span;
+}
