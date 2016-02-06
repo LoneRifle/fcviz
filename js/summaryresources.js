e20d7f5357e50cca23671679d7a1e9c5d4cc8605
LoneRifle <LoneRifle@users.noreply.github.com>
2016-02-06 16:47:25 +0800

    Include pathseg polyfill for c3.js
    Chrome 48 removed the SVG interface in
    https://code.google.com/p/chromium/issues/detail?id=539385 . Since c3.js
    has had no release since March 2015, a pathseg polyfill has to be
    introduced to redirect the old pathSegList interface to the new Path API
    found in the SVG spec.
    
    pathseg.js was taken from https://github.com/progers/pathseg .

--- js/summaryresources.js
+++ js/summaryresources.js
@@ -1,1 +1,1 @@
-load({css:["css/fcviz.css", "css/c3.min.css"], js:["js/d3.min.js", "js/c3.min.js", "js/summary.js"]})
+load({css:["css/fcviz.css", "css/c3.min.css"], js:["js/pathseg.js", "js/d3.min.js", "js/c3.min.js", "js/summary.js"]})
