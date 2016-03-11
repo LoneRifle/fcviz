21aeaca04723167e725ed34ac08fe80e2506f792
LoneRifle <LoneRifle@users.noreply.github.com>
2016-03-11 18:29:30 +0800

    Add shift-clicking for delisting of loan parts

---
+++ js/forsale.js
@@ -0,0 +1,10 @@
+/**
+ * FCViz 
+ * Payload script for the for sale iframe within the Sell page
+ */
+
+window.lastChecked = null;
+
+addMultiCheckToAngularModule(angular.module("SellLoanParts"));
+
+resumeBootstrapAndInjectEnableShortcuts();
---
+++ js/forsaleresources.js
@@ -0,0 +1,1 @@
+loadAtTarget({css:["css/fcviz.css"], js:["js/deferbootstrap.js", "js/addmultichecktoangularmodule.js", "js/forsale.js"]},document.body)
