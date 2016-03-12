bff4c7c6896e0c6a818fa7012fac13fcb82264e8
LoneRifle <LoneRifle@users.noreply.github.com>
2016-03-12 16:22:02 +0800

    begin implementation of a function that processes filed accounts data

--- js/consolidateaccounts.js
+++ js/consolidateaccounts.js
@@ -5,7 +5,25 @@ window.triggerConsolidateAccountsWidgets = function (id) {
     return;
   }
   
-  window.originalLoanAccounts = $("table.brand:nth-child(2) tr td:not(:first-child)");
-  window.originalLoanDates = $("table.brand:nth-child(2) tr th:not(:first-child)");
-  console.log(originalLoanAccounts);
+  var originalLoanAccounts = $("table.brand:nth-child(2) tr td:not(:first-child)");
+  var originalLoanDates = $("table.brand:nth-child(2) tr th:not(:first-child)");
+  window.originalLoanData = makeLoanData(originalLoanAccounts, originalLoanDates);
+  
+
+  
+  console.log(originalLoanData);
+}
+
+function makeLoanData(accounts, dates) {
+  var loanData = { dates:[] };
+  
+  var loanRawData = $.merge($.merge(accounts.slice(0,3), dates), accounts.slice(3));
+  console.log(loanRawData);
+  
+  dates.each(function(i, d){
+    var dateStr = $(d).html().trim();
+    loanData.dates.unshift(dateStr);
+    
+  });
+  return loanData;
 }
