dab37611fc51f65ddc0df59067adb4401620f232
LoneRifle <alwyn.tan@gmail.com>
2017-03-07 21:16:26 +0800

    Capture, restore search when enable sell shortcuts

--- js/deferbootstrap.js
+++ js/deferbootstrap.js
@@ -5,12 +5,28 @@ function resumeBootstrapAndInjectEnableShortcuts() {
   if (angular.resumeBootstrap) {
     angular.resumeBootstrap();
   }
-
+  
+  const indexOfQueryParam = window.location.href.indexOf('?query=');
+  if (indexOfQueryParam != -1){
+    const queryTerms = unescape(window.location.href.substring(indexOfQueryParam + 7));
+    angular.element(document.querySelector("input")).val(queryTerms);
+    history.replaceState( {} , null, window.location.href.substring(0, indexOfQueryParam) );
+  }
+  
   //Add a link that reloads the page, so that we can pick up the buttons
   var reload = angular.element(document.createElement("span"));
-  reload.attr("class","repay_change_data")
+  reload
+    .attr("class","repay_change_data")
     .html("Enable Shortcuts")
-    .on("click", function(){location.reload();});
+    .on("click", function() {
+      const queryText = angular.element(document.querySelector("input")).val();
+      if (queryText) {
+        window.location.replace(window.location.href + '?query=' + queryText);
+      } else {
+        window.location.reload();
+      }
+    });
+    
   angular.element(document.querySelector("p"))
     .append("&nbsp;").append(reload);
 }
