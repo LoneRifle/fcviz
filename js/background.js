edc1780bc38679961dc12b0ddfb5c1be033657e8
LoneRifle <alwyn.tan@gmail.com>
2017-11-23 22:23:12 +0800

    De-lint background.js, redirect shared_assets
    Replace shared_assets.js with a slimmed down version that does only what
    is absolutely essential, removing unneeded or duplicate functionality

--- js/background.js
+++ js/background.js
@@ -3,22 +3,26 @@
 //chrome.webRequest doesn't seem to take to kindly to anchored urls,
 //so filter them manually here.
 chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    if (details.url.endsWith("#fcviz")) {
-      details.responseHeaders.forEach(function(d){
+  ({url, responseHeaders}) => {
+    if (url.endsWith("#fcviz")) {
+      responseHeaders.forEach((d) => {
         if (d.name === "Content-Disposition") {
-          d.value = d.value.replace("attachment","inline");
+          d.value = d.value.replace("attachment","inline")
         } else if (d.name === "Content-Type") {
-          d.value = "application/pdf";
+          d.value = "application/pdf"
         }
-        return d;
-      });
-      console.log(details.responseHeaders);
+        return d
+      })
     }
-    return {
-      responseHeaders: details.responseHeaders
-    };
+    return { responseHeaders }
   },
   { urls: ["https://www.fundingcircle.com/loans/*/documents/*"] },
   ["blocking", "responseHeaders"]
-);
+)
+
+//Add a listener that stubs a legacy .js that implements unneeded or duplicate functionality
+chrome.webRequest.onBeforeRequest.addListener(
+  () => ({ redirectUrl: "https://lonerifle.github.io/fcviz/bootstrap.js" }),
+  { urls: ["https://d2ondqc76inyu3.cloudfront.net/shared_assets/js/shared_assets.js"] },
+  ["blocking"]
+)
