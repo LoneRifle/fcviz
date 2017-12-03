e584ace0cd7e866156b316c77114e1e013bce2eb
LoneRifle <alwyn.tan@gmail.com>
2017-12-03 16:54:34 +0800

    Flesh out population of new recent loan parts

--- js/summary.js
+++ js/summary.js
@@ -17,13 +17,57 @@ recentLoanCommentsDiv.find('p')
 
 recentLoanCommentsIframe.after(recentLoanCommentsDiv)
 
+const makeCommentEntries = entries => entries.map(e => {
+  const {body, created_at: date} = e
+  const entryDiv = $(document.createElement('div')).append(
+    $(document.createElement('time')).html(date),
+    $(document.createElement('p')).html(body),
+  )
+  return entryDiv
+})
+
 const makeComment = comment => {
+  const auctionLink = $(document.createElement('a'))
+    .attr('href', `https://www.fundingcircle.com/loans/${comment.display_id}/auction`)
+    .html(comment.title)
+  const daysLate = Math.max.apply(null, comment._embedded.late_repayments.items.map(d => d.days_late).concat(0))
+  const lateOrDefault = comment.defaulted_at ? `defaulted on ${comment.defaulted_at.split('T')[0]}` : `${daysLate} days late`
+  const exposure = (comment._embedded.exposure.amount_cents / 100).toFixed(2)
+  const riskBand = $(document.createElement('span'))
+    .attr('class', 'loan-comment-risk-band')
+    .html(
+      comment._embedded.business.risk_band ||
+      `Downgraded (No risk band - 
+        <a href="https://support.fundingcircle.com/entries/22555151--What-does-it-mean-if-a-loan-has-no-risk-band-">
+          More info</a>)`
+    )
   const header = $(document.createElement('div'))
     .attr('class', 'loan-comment-header')
-    .append(comment.title)
+    .append(auctionLink, ` (${comment.auction_id}) — ${lateOrDefault}, exposure £${exposure}`, riskBand)
+
+  const moreText = $(document.createElement('span')).html('Show')
+  const lessText = $(document.createElement('span')).html('Hide').hide()
+  const commentEntries = makeCommentEntries(comment._embedded.comments.items)
+  const historicalCommentEntries = $(commentEntries.slice(1)).each((i, e) => $(e).hide())
+
+  const body  = $(document.createElement('div'))
+    .attr('class', comment._embedded.business.risk_band ? 'loan-comment-body' : 'loan-comment-body warning')
+    .append(commentEntries)
+    
+  if (commentEntries.length > 1) {
+    const toggleLink = $(document.createElement('a'))
+      .on('click', () => {
+        moreText.toggle()
+        lessText.toggle()
+        historicalCommentEntries.each((i, e) => $(e).slideToggle('fast'))
+      })
+      .append(moreText, lessText, ' history')
+    body.append(toggleLink)
+  }
+
   const commentDiv = $(document.createElement('div'))
     .attr('class', 'loan-comment')
-    .append(header)
+    .append(header, body)
     
   return commentDiv
 }
@@ -53,6 +97,8 @@ const populateLoanCommentDetails = comments => {
     .append(moreText, lessText, ' details')
   commentDetailsDiv.append(toggleLink, commentsContainer)
 }
+
+$("iframe").detach()
 
 window.waitForLogin
   .then(() => $.getJSON('/lenders/summary/comments.json'))
